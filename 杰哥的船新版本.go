79733da13dae42ac5565aff3bf1d4d655cb95e1f
Terandial <114346416+Terandial@users.noreply.github.com>
2022-10-26 22:08:08 +0800

    Add files via upload

---
+++ 杰哥的船新版本.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func 你干嘛() {
+	println("有没有可能，我最近在锻炼身体")
+}
+
+func 电动1() {
+	fmt.Println("死了啦！都是你害的！")
+
+}
+func 电动2() {
+	time.Sleep(5 * time.Second)
+	fmt.Println("我阿妈超凶的！")
+}
+
+func main() {
+	var 杰哥超爱的 杰哥超爱的
+	杰哥超爱的 = 杰哥给你康点好的
+
+	电动1()
+	电动2()
+	杰哥超爱的()
+	杰哥超爱的 = 你干嘛
+	杰哥超爱的()
+
+}
+func 杰哥给你康点好的() {
+	time.Sleep(5 * time.Second)
+	fmt.Println("登dua郎")
+
+}
+
+type 杰哥超爱的 func()
